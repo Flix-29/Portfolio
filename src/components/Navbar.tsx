e5c80783dc1700b01a2eb402c2fd6285643dc482
flix29 <flix.1895@web.de>
2025-03-30 22:13:08 +0200

    added home link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,10 +48,12 @@ export default function Navbar() {
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex shrink-0 items-center">
+                            <a href="#hero" className="flex items-center">
                             <img
                                 src="/icon.png"
                                 className="h-8 w-auto"
                             />
+                            </a>
                         </div>
                         <div className="hidden sm:ml-6 sm:block">
                             <div className="flex space-x-4">
